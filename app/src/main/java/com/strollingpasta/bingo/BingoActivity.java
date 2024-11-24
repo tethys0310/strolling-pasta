e933676a3a2d6df99619986947e7c02d0cfddcb8
tethys0310 <tethys0310@gmail.com>
2024-11-24 18:17:10 +0900

    빙고액티비티 뷰 바인더로 수정

--- app/src/main/java/com/strollingpasta/bingo/BingoActivity.java
+++ app/src/main/java/com/strollingpasta/bingo/BingoActivity.java
@@ -8,17 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.strollingpasta.bingo.databinding.ActivityBingoBinding;
+import com.strollingpasta.bingo.databinding.ActivityMainBinding;
+
 public class BingoActivity extends AppCompatActivity {
+
+    // 뷰 바인딩
+    private ActivityBingoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bingo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // 뷰 바인딩
+        binding = ActivityBingoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
 }
